4b2bc393140d8be9ca04e38be612f27c7ce52489
szakaljel <szakaljel@gmail.com>
2020-08-05 21:33:53 +0200

    sprint 3

--- sprint_3/3/kube.data.sql
+++ sprint_3/3/kube.data.sql
@@ -1,1 +1,8 @@
-INSERT INTO `test` VALUES (1,'test1'),(2,'test2'),(3,'test3'),(4,'test4'),(5,'test5') ON DUPLICATE KEY UPDATE;
+CREATE TABLE IF NOT EXISTS `test` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+INSERT INTO `test` (id, name) VALUES (1,'test1'),(2,'test2'),(3,'test3'),(4,'test4'),(5,'test5')
+    ON DUPLICATE KEY UPDATE id=id, name=name;
