1d07608f898d1d791b449b892be19dfe47bd2e98
szakaljel <szakaljel@gmail.com>
2020-08-27 00:14:31 +0200

    update

--- sprint_5/data/1_init.sql
+++ sprint_5/data/1_init.sql
@@ -4,3 +4,8 @@ create table cities (
     population int not null,
     time_zone varchar(50) not null
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8;
+
+insert into cities (id, name, population, time_zone) values
+(1, 'Gdańsk', 470900, 'Europe/Warsaw'),
+(2, 'Paryż', 2243833, 'Europe/Paris'),
+(3, 'Lisbona', 547631, 'Europe/Lisbon');
--- sprint_5/geo/data/data.sql
+++
@@ -1,4 +0,0 @@
-insert into cities (id, name, population, time_zone) values
-    (1, 'Gdańsk', 470900, 'Europe/Warsaw'),
-    (2, 'Paryż', 2243833, 'Europe/Paris'),
-    (3, 'Lisbona', 547631, 'Europe/Lisbon');
